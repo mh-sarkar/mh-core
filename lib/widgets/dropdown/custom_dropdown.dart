125595e46aad231c6a4d1064264fb7a0155dbee9
mh-sarkar-wiztecbd <mhsarkar.cse@gmail.com>
2023-05-23 16:58:59 +0600

    feat: add custom text field + fix: custom dropdown widget

--- lib/widgets/dropdown/custom_dropdown.dart
+++ lib/widgets/dropdown/custom_dropdown.dart
@@ -13,6 +13,12 @@ class TitleDropdown extends StatelessWidget {
   final Function onChange;
   final double? height;
   final double? width;
+  final double? marginHorizontal;
+  final double? marginVertical;
+  final double? marginTop;
+  final double? marginLeft;
+  final double? marginRight;
+  final double? marginBottom;
   final double? circularBorderRadius;
   final String? hintText;
   final Color? textColor;
@@ -20,16 +26,19 @@ class TitleDropdown extends StatelessWidget {
   final Color? borderColor;
   final Color? bgColor;
   final Color? itemColor;
+  final Color? fillColor;
+  final Color? rightIconBgColor;
   final TextStyle? hintTextStyle;
   final TextStyle? textStyle;
-  final double? horizontalPadding;
-  bool isFillColor;
-  bool isBorder;
-  bool isTitle;
-  bool isRightIcon;
+  final IconData? dropDownIconData;
+  final double? horizontalContentPadding;
+  final bool isFillColor;
+  final bool isBorder;
+  final bool isTitle;
+  final bool isRightIcon;
   final DropdownListType? type;
 
-  TitleDropdown({
+  const TitleDropdown({
     super.key,
     required this.dwItems,
     this.title,
@@ -46,7 +55,7 @@ class TitleDropdown extends StatelessWidget {
     this.circularBorderRadius,
     this.hintTextStyle,
     this.textStyle,
-    this.horizontalPadding,
+    this.horizontalContentPadding,
     this.isFillColor = false,
     this.isBorder = true,
     this.isTitle = true,
@@ -54,90 +63,118 @@ class TitleDropdown extends StatelessWidget {
     this.viewKey,
     this.valueKey,
     this.type = DropdownListType.none,
+    this.marginHorizontal,
+    this.marginVertical,
+    this.marginTop,
+    this.marginLeft,
+    this.marginRight,
+    this.marginBottom,
+    this.fillColor,
+    this.dropDownIconData,
+    this.rightIconBgColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (isTitle)
-          Text(title ?? '',
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
-                color: Colors.black,
-              )),
-        if (isTitle) space4C,
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: horizontalPadding ?? 16),
-          width: width ?? double.infinity,
-          height: height ?? 56,
-          decoration: BoxDecoration(
-              color: const Color(0xffFCFCFA),
-              border: isBorder ? Border.all(color: borderColor ?? Colors.black.withOpacity(.14), width: 1) : null,
-              borderRadius: BorderRadius.circular(circularBorderRadius ?? 10),
-              boxShadow: [
-                BoxShadow(
-                    offset: const Offset(0.0, 4.0), blurRadius: 16, color: const Color(0xff25251C).withOpacity(.05))
-              ]),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              dropdownColor: const Color(0xffFCFCFA),
-              hint: Container(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  hintText ?? '',
-                  style: hintTextStyle ??
-                      TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                        color: Colors.black.withOpacity(.50),
-                      ),
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: marginHorizontal ?? 16,
+        vertical: marginVertical ?? 12,
+      ).copyWith(
+        left: marginLeft ?? marginHorizontal ?? 16,
+        top: marginTop ?? marginVertical ?? 12,
+        right: marginRight ?? marginHorizontal ?? 16,
+        bottom: marginBottom ?? marginVertical ?? 12,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (isTitle)
+            Text(title ?? '',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: Colors.black,
+                )),
+          if (isTitle) space2C,
+          Container(
+            // padding: EdgeInsets.symmetric(horizontal: horizontalPadding ?? 16),
+            width: width ?? double.infinity,
+            height: height ?? 44,
+            decoration: BoxDecoration(
+                color: fillColor ?? const Color(0xffFCFCFA),
+                border: isBorder ? Border.all(color: borderColor ?? Colors.black.withOpacity(.14), width: 1) : null,
+                borderRadius: BorderRadius.circular(circularBorderRadius ?? 5),
+                boxShadow: [
+                  BoxShadow(
+                      offset: const Offset(0.0, 4.0), blurRadius: 16, color: const Color(0xff25251C).withOpacity(.05))
+                ]),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(circularBorderRadius ?? 5),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  dropdownColor: const Color(0xffFCFCFA),
+                  hint: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: horizontalContentPadding ?? 8),
+                    child: Text(
+                      hintText ?? '',
+                      style: hintTextStyle ??
+                          TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(.50),
+                          ),
+                    ),
+                  ),
+                  icon: Container(
+                    margin: EdgeInsets.only(right: horizontalContentPadding ?? 8),
+                    height: (height ?? 44) - 8,
+                    width: (height ?? 44) - 8,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: rightIconBgColor ?? CustomColor.kPrimaryColor,
+                    ),
+                    child: Center(
+                      child: Icon(dropDownIconData ?? Icons.keyboard_arrow_down_outlined,
+                          color: rightIconColor ?? CustomColor.kTextColor),
+                    ),
+                  ),
+                  isExpanded: true,
+                  isDense: true,
+                  value: dwValue,
+                  onChanged: (newValue) {
+                    onChange(newValue);
+                  },
+                  items: dwItems
+                      .map((e) => type == DropdownListType.object ? e.toJson() : e)
+                      .toList()
+                      .map(
+                        (item) => DropdownMenuItem<String>(
+                          value: type == DropdownListType.none ? item : item[valueKey],
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              type == DropdownListType.none ? item : item[viewKey],
+                              style: textStyle ??
+                                  const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 12,
+                                  ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
                 ),
               ),
-              icon: isRightIcon
-                  ? Container(
-                      height: 36,
-                      width: 36,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: CustomColor.kPrimaryColor,
-                      ),
-                      child: const Icon(Icons.keyboard_arrow_down_outlined, color: Colors.white),
-                    )
-                  : Image.asset('images/icon/arrow_down.png', color: rightIconColor),
-              isExpanded: true,
-              isDense: true,
-              value: dwValue,
-              onChanged: (newValue) {
-                onChange(newValue);
-              },
-              items: dwItems
-                  .map(
-                    (item) => DropdownMenuItem<String>(
-                      value: type == DropdownListType.none ? item : item[valueKey],
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          type == DropdownListType.none ? item : item[viewKey],
-                          style: textStyle ??
-                              const TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w600,
-                                fontSize: 12,
-                              ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ),
-                    ),
-                  )
-                  .toList(),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/textfield/custom_textfield.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:mh_core/utils/color/custom_color.dart';
+
+import '../../utils/constant.dart';
+
+class CustomTextField extends StatefulWidget {
+  final double? height;
+  final double? width;
+  final double? borderRadius;
+  final String? hintText;
+  final double? hintSize;
+  final Color? hintColor;
+  final Color? focusColor;
+  final Widget? prefixWidget;
+  final Color? enableBorderColor;
+  final FontWeight? hintFontWeight;
+  final double? textSize;
+  final Color? textColor;
+  final Color? fillColor;
+  final FontWeight? textFontWeight;
+  final String? labelText;
+  final double? labelSize;
+  final Color? labelColor;
+  final FontWeight? labelFontWeight;
+  final String? errorMessage;
+  final int maxLine;
+  final TextEditingController? controller;
+  final double? marginHorizontal;
+  final double? marginVertical;
+  final Function(String)? onSubmitted;
+  final bool? isPassword;
+  final bool? obscureText;
+  final bool? isInvalid;
+  final bool isLabelSeparated;
+  final Widget? suffixIcon;
+  final FocusNode? focusNode;
+  final TextInputType? keyboardType;
+  final bool? isRequired;
+  final bool isEnable;
+  final ValueChanged<String>? onChanged;
+  final double? marginTop;
+  final double? marginLeft;
+  final double? marginRight;
+  final double? marginBottom;
+
+  const CustomTextField({
+    Key? key,
+    this.height,
+    this.width,
+    this.borderRadius,
+    this.hintText,
+    this.focusColor,
+    this.hintSize,
+    this.fillColor,
+    this.enableBorderColor,
+    this.prefixWidget,
+    this.hintColor,
+    this.hintFontWeight,
+    this.textColor,
+    this.textSize,
+    this.textFontWeight,
+    this.labelText,
+    this.labelColor,
+    this.labelFontWeight,
+    this.labelSize,
+    this.errorMessage,
+    this.onChanged,
+    this.isEnable = true,
+    this.maxLine = 1,
+    this.controller,
+    this.marginHorizontal,
+    this.suffixIcon,
+    this.marginVertical,
+    this.isPassword = false,
+    this.isLabelSeparated = true,
+    this.obscureText = false,
+    this.isInvalid = false,
+    this.focusNode,
+    this.keyboardType,
+    this.isRequired,
+    this.onSubmitted,
+    this.marginTop,
+    this.marginLeft,
+    this.marginRight,
+    this.marginBottom,
+  }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = false;
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText!;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: widget.marginHorizontal ?? 16,
+          vertical: widget.marginVertical ?? 12,
+        ).copyWith(
+          left: widget.marginLeft ?? widget.marginHorizontal ?? 16,
+          top: widget.marginTop ?? widget.marginVertical ?? 12,
+          right: widget.marginRight ?? widget.marginHorizontal ?? 16,
+          bottom: widget.marginBottom ?? widget.marginVertical ?? 12,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (widget.isLabelSeparated && widget.labelText != null)
+              Text(widget.labelText ?? '',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    color: Colors.black,
+                  )),
+            if (widget.isLabelSeparated && widget.labelText != null) space2C,
+            Container(
+              color: widget.fillColor,
+              height: widget.maxLine > 1 ? null : widget.height ?? 44,
+              width: widget.width ?? size.width,
+              // margin: EdgeInsets.symmetric(
+              //   horizontal: widget.marginHorizontal ?? 16,
+              //   vertical: widget.marginVertical ?? 12,
+              // ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      enabled: widget.isEnable,
+                      focusNode: widget.focusNode,
+                      cursorColor: Colors.black,
+                      obscureText: _obscureText,
+                      controller: widget.controller,
+                      maxLines: widget.maxLine ?? 1,
+                      keyboardType: widget.keyboardType,
+                      onFieldSubmitted: widget.onSubmitted,
+                      style: TextStyle(
+                        color: widget.textColor ?? Colors.black,
+                        fontSize: widget.textSize ?? 14,
+                        fontWeight: widget.textFontWeight ?? FontWeight.w500,
+                      ),
+                      onChanged: widget.onChanged,
+                      decoration: InputDecoration(
+                          prefixIcon: widget.prefixWidget,
+                          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                          hintText: widget.hintText,
+                          hintStyle: TextStyle(
+                            color: widget.hintColor ?? const Color(0xff000000).withOpacity(0.40),
+                            fontSize: widget.hintSize ?? 14,
+                            fontWeight: widget.hintFontWeight ?? FontWeight.w400,
+                          ),
+                          labelStyle: TextStyle(
+                            color: widget.labelColor ?? Colors.black.withOpacity(.7),
+                            fontSize: widget.labelSize ?? 12,
+                            fontWeight: widget.labelFontWeight ?? FontWeight.w400,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                            borderSide: BorderSide(
+                              color: widget.focusColor ?? const Color(0xffD9D9D9),
+                              width: 1,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                            borderSide: BorderSide(
+                              color: widget.enableBorderColor ?? const Color(0xffD9D9D9),
+                              width: 1,
+                            ),
+                          ),
+                          labelText: widget.isLabelSeparated ? null : widget.labelText,
+                          isDense: true,
+                          suffixIcon: widget.isPassword!
+                              ? InkWell(
+                                  onTap: () {
+                                    toogle();
+                                  },
+                                  child: Icon(_obscureText ? Icons.visibility_off : Icons.visibility,
+                                      color: const Color(0xff484848)),
+                                )
+                              : widget.suffixIcon),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (widget.errorMessage != null)
+              Column(
+                children: [
+                  const SizedBox(height: 12),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      widget.errorMessage!,
+                      style: TextStyle(
+                        color: CustomColor.kPrimaryColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ));
+  }
+
+  void toogle() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+}
