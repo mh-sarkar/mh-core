cbc8ba80b334bc07646bd0ce57d9d271d6cc8e17
mh-sarkar-wiztecbd <mhsarkar.cse@gmail.com>
2023-05-30 15:24:45 +0600

    feat: time calculation, fix rating widget

--- lib/utils/string_utils.dart
+++ lib/utils/string_utils.dart
@@ -8,3 +8,43 @@ String getTitle(String englishName, String banglaName) {
 dynamic getModelInfo(String id, List listData) {
   return listData.where((e) => e.id == id).toList()[0];
 }
+
+String getTime(String data) {
+  if (data.length == 3) {
+    return "${data.replaceAll(':', '')} m 00 s";
+  } else if (data.length == 5) {
+    return "${data.substring(0, data.indexOf(':'))} m ${data.substring(data.indexOf(':') + 1, data.length)} s";
+  } else {
+    return "${data.substring(0, data.indexOf(' hour,'))} h ${data.substring(data.indexOf('hour,') + 6, data.indexOf(':'))} m ${data.substring(data.indexOf(':') + 1, data.length)} s";
+  }
+}
+
+String getTotalTime(List<String> list) {
+  int hour = 0;
+  int min = 0;
+  int sec = 0;
+
+  for (var element in list) {
+    final time = getTime(element);
+    if (time.contains('h')) {
+      hour += int.parse(time.substring(0, time.indexOf('h')));
+    }
+    if (time.contains('m')) {
+      min += int.parse(time.substring(time.indexOf('h') + 1, time.indexOf('m')));
+    }
+    if (time.contains('s')) {
+      sec += int.parse(time.substring(time.indexOf('m') + 1, time.indexOf('s')));
+    }
+  }
+
+  if (sec > 59) {
+    min += (sec / 60).floor();
+    sec = sec % 60;
+  }
+  if (min > 59) {
+    hour += (min / 60).floor();
+    min = min % 60;
+  }
+
+  return "$hour h $min m $sec s";
+}
--- lib/widgets/rating/custom_rating.dart
+++ lib/widgets/rating/custom_rating.dart
@@ -3,21 +3,30 @@ import 'package:mh_core/utils/constant.dart';
 
 class RatingWidget extends StatelessWidget {
   final double rating;
+  final bool showRatingValue;
+  final MainAxisAlignment? mainAxisAlignment;
+  final CrossAxisAlignment? crossAxisAlignment;
 
   const RatingWidget({
     super.key,
     required this.rating,
+    this.showRatingValue = true,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment!,
+      crossAxisAlignment: crossAxisAlignment!,
       children: [
-        Text(
-          rating.toStringAsFixed(1),
-          style: const TextStyle(color: Color(0xffFFA873), fontSize: 16, fontWeight: FontWeight.w600),
-        ),
-        space1R,
+        if (showRatingValue)
+          Text(
+            rating.toStringAsFixed(1),
+            style: const TextStyle(color: Color(0xffFFA873), fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+        if (showRatingValue) space1R,
         Icon(rating > 0 && rating < 1 ? Icons.star_half : Icons.star,
             color: rating > 0 ? const Color(0xffFFA873) : const Color(0xff8C8FA5), size: 14),
         Icon(rating > 1 && rating < 2 ? Icons.star_half : Icons.star,
