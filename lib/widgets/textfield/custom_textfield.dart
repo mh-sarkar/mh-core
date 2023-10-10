28d16ef1bb529abdd97c3a56f50fbb6b07dc52c9
mh-sarkar-wiztecbd <mh-sarkar-wiztecbd>
2023-10-10 19:48:17 +0600

    fix: custom textfield error color fix

--- lib/widgets/textfield/custom_textfield.dart
+++ lib/widgets/textfield/custom_textfield.dart
@@ -44,6 +44,8 @@ class CustomTextField extends StatefulWidget {
   final double? marginBottom;
   final double? borderWidth;
   final Color? suffixIconColor;
+  final Color? errorFillColor;
+  final Color? errorColor;
 
   const CustomTextField({
     Key? key,
@@ -87,6 +89,8 @@ class CustomTextField extends StatefulWidget {
     this.marginBottom,
     this.borderWidth,
     this.suffixIconColor,
+    this.errorColor,
+    this.errorFillColor,
   }) : super(key: key);
 
   @override
@@ -106,124 +110,141 @@ class _CustomTextFieldState extends State<CustomTextField> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-        margin: EdgeInsets.symmetric(
-          horizontal: widget.marginHorizontal ?? 16,
-          vertical: widget.marginVertical ?? 12,
-        ).copyWith(
-          left: widget.marginLeft ?? widget.marginHorizontal ?? 16,
-          top: widget.marginTop ?? widget.marginVertical ?? 12,
-          right: widget.marginRight ?? widget.marginHorizontal ?? 16,
-          bottom: widget.marginBottom ?? widget.marginVertical ?? 12,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (widget.isLabelSeparated && widget.labelText != null)
-              Row(
-                children: [
-                  Text(widget.labelText ?? '',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 14,
-                        color: widget.labelColor ?? Colors.black,
-                      )),
-                  if (widget.isRequired)
-                    Text(
-                      ' *',
-                      style: TextStyle(color: Colors.red, fontSize: widget.labelSize, fontWeight: widget.labelFontWeight),
-                    )
-                ],
-              ),
-            if (widget.isLabelSeparated && widget.labelText != null) space2C,
-            SizedBox(
-              height: widget.maxLine == null && (widget.maxLine != null && widget.maxLine! > 1) ? null : widget.height ?? 44,
-              width: widget.width ?? size.width,
-              // margin: EdgeInsets.symmetric(
-              //   horizontal: widget.marginHorizontal ?? 16,
-              //   vertical: widget.marginVertical ?? 12,
-              // ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      enabled: widget.isEnable,
-                      focusNode: widget.focusNode,
-                      cursorColor: Colors.black,
-                      obscureText: _obscureText,
-                      controller: widget.controller,
-                      maxLines: widget.maxLine,
-                      keyboardType: widget.keyboardType,
-                      onFieldSubmitted: widget.onSubmitted,
-                      style: TextStyle(
-                        color: widget.textColor ?? Colors.black,
-                        fontSize: widget.textSize ?? 14,
-                        fontWeight: widget.textFontWeight ?? FontWeight.w500,
-                      ),
-                      onChanged: widget.onChanged,
-                      decoration: InputDecoration(
-                          prefixIcon: widget.prefixWidget,
-                          fillColor: widget.fillColor,
-                          filled: widget.fillColor != null,
-                          contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-                          hintText: widget.hintText,
-                          hintStyle: TextStyle(
-                            color: widget.hintColor ?? const Color(0xff000000).withOpacity(0.40),
-                            fontSize: widget.hintSize ?? 14,
-                            fontWeight: widget.hintFontWeight ?? FontWeight.w400,
-                          ),
-                          labelStyle: TextStyle(
-                            color: widget.labelColor ?? Colors.black.withOpacity(.7),
-                            fontSize: widget.labelSize ?? 12,
-                            fontWeight: widget.labelFontWeight ?? FontWeight.w400,
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
-                            borderSide: BorderSide(
-                              color: widget.focusColor ?? const Color(0xffD9D9D9),
-                              width: widget.borderWidth ?? 1,
-                            ),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
-                            borderSide: BorderSide(
-                              color: widget.enableBorderColor ?? const Color(0xffD9D9D9),
-                              width: widget.borderWidth ?? 1,
-                            ),
-                          ),
-                          labelText: widget.isLabelSeparated ? null : widget.labelText,
-                          isDense: true,
-                          suffixIcon: widget.isPassword!
-                              ? InkWell(
-                                  onTap: () {
-                                    toogle();
-                                  },
-                                  child: Icon(_obscureText ? Icons.visibility_off : Icons.visibility, color: widget.suffixIconColor ?? const Color(0xff484848)),
-                                )
-                              : widget.suffixIcon),
-                    ),
-                  ),
-                ],
-              ),
+      margin: EdgeInsets.symmetric(
+        horizontal: widget.marginHorizontal ?? 16,
+        vertical: widget.marginVertical ?? 12,
+      ).copyWith(
+        left: widget.marginLeft ?? widget.marginHorizontal ?? 16,
+        top: widget.marginTop ?? widget.marginVertical ?? 12,
+        right: widget.marginRight ?? widget.marginHorizontal ?? 16,
+        bottom: widget.marginBottom ?? widget.marginVertical ?? 12,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (widget.isLabelSeparated && widget.labelText != null)
+            Row(
+              children: [
+                Text(widget.labelText ?? '',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: widget.labelColor ?? Colors.black,
+                    )),
+                if (widget.isRequired)
+                  Text(
+                    ' *',
+                    style: TextStyle(color: Colors.red, fontSize: widget.labelSize, fontWeight: widget.labelFontWeight),
+                  )
+              ],
             ),
-            if (widget.errorMessage != null)
-              Column(
-                children: [
-                  const SizedBox(height: 12),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      widget.errorMessage!,
-                      style: TextStyle(
-                        color: CustomColor.kPrimaryColor,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600,
-                      ),
+          if (widget.isLabelSeparated && widget.labelText != null) space2C,
+          SizedBox(
+            height: widget.maxLine == null && (widget.maxLine != null && widget.maxLine! > 1) ? null : widget.height ?? 44,
+            width: widget.width ?? size.width,
+            // margin: EdgeInsets.symmetric(
+            //   horizontal: widget.marginHorizontal ?? 16,
+            //   vertical: widget.marginVertical ?? 12,
+            // ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    enabled: widget.isEnable,
+                    focusNode: widget.focusNode,
+                    cursorColor: Colors.black,
+                    obscureText: _obscureText,
+                    controller: widget.controller,
+                    maxLines: widget.maxLine,
+                    keyboardType: widget.keyboardType,
+                    onFieldSubmitted: widget.onSubmitted,
+                    style: TextStyle(
+                      color: widget.textColor ?? Colors.black,
+                      fontSize: widget.textSize ?? 14,
+                      fontWeight: widget.textFontWeight ?? FontWeight.w500,
+                    ),
+                    onChanged: widget.onChanged,
+                    decoration: InputDecoration(
+                        prefixIcon: widget.prefixWidget,
+                        fillColor: widget.errorMessage != null ? widget.errorFillColor ?? const Color(0xffFFF5F6) : widget.fillColor,
+                        filled: widget.fillColor != null,
+                        contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                        hintText: widget.hintText,
+                        hintStyle: TextStyle(
+                          color: widget.hintColor ?? const Color(0xff000000).withOpacity(0.40),
+                          fontSize: widget.hintSize ?? 14,
+                          fontWeight: widget.hintFontWeight ?? FontWeight.w400,
+                        ),
+                        labelStyle: TextStyle(
+                          color: widget.labelColor ?? Colors.black.withOpacity(.7),
+                          fontSize: widget.labelSize ?? 12,
+                          fontWeight: widget.labelFontWeight ?? FontWeight.w400,
+                        ),
+                        focusedBorder: widget.errorMessage != null
+                            ? OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                                borderSide: BorderSide(
+                                  color: widget.errorColor ?? Colors.red,
+                                  width: widget.borderWidth ?? 1,
+                                ),
+                              )
+                            : OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                                borderSide: BorderSide(
+                                  color: widget.focusColor ?? const Color(0xffD9D9D9),
+                                  width: widget.borderWidth ?? 1,
+                                ),
+                              ),
+                        enabledBorder: widget.errorMessage != null
+                            ? OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                                borderSide: BorderSide(
+                                  color: widget.errorColor ?? Colors.red,
+                                  width: widget.borderWidth ?? 1,
+                                ),
+                              )
+                            : OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(widget.borderRadius ?? 5),
+                                borderSide: BorderSide(
+                                  color: widget.enableBorderColor ?? const Color(0xffD9D9D9),
+                                  width: widget.borderWidth ?? 1,
+                                ),
+                              ),
+                        labelText: widget.isLabelSeparated ? null : widget.labelText,
+                        isDense: true,
+                        suffixIcon: widget.isPassword
+                            ? InkWell(
+                                onTap: () {
+                                  toogle();
+                                },
+                                child: Icon(_obscureText ? Icons.visibility_off : Icons.visibility, color: widget.suffixIconColor ?? const Color(0xff484848)),
+                              )
+                            : widget.suffixIcon),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (widget.errorMessage != null)
+            Column(
+              children: [
+                const SizedBox(height: 12),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.errorMessage!,
+                    style: TextStyle(
+                      color: widget.errorColor ?? Colors.red,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ],
-              ),
-          ],
-        ));
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
   }
 
   void toogle() {
